3b9cfc9c1afb2e7d0ab877c9ea1eadde6dce2f72
SL <sl@Siens-MBP.lan>
2020-08-01 18:40:58 -0400

    clean up

--- shopify/slate-master/src/scripts/theme.js
+++ shopify/slate-master/src/scripts/theme.js
@@ -1,12 +1,2 @@
 import _ from 'lodash';
 import '../styles/theme.scss';
-
-function component() {
-    const element = document.createElement('div');
-    // Lodash, currently included via a script, is required for this line to work
-    element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-  
-    return element;
-  }
-  
-  document.body.appendChild(component());
