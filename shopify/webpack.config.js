06ed0eec65c28c2c7cfdf8cb8ed31b530adb8b8f
tzuhsien liu <tzuhsien@onerockwell.com>
2020-08-02 23:05:59 -0400

    add font file, webpack load fonts

--- shopify/webpack.config.js
+++ shopify/webpack.config.js
@@ -4,11 +4,12 @@ const TerserPlugin = require('terser-webpack-plugin');
 const UglifyJSPlugin = require('uglifyjs-webpack-plugin');
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin');
+const SpriteLoaderPlugin = require('svg-sprite-loader/plugin');
 
 module.exports = {
-  entry: './src/scripts/theme.js',
+  entry: 'src/scripts/theme.js',
   output: {
-    path: path.resolve(__dirname, './src/assets'), // outputs bundled .js and .scss.liquid into shopify's assets folder
+    path: path.resolve(__dirname, 'src/assets'), // outputs bundled .js and .scss.liquid into shopify's assets folder
     filename: 'theme.js',
   },
   optimization: {
@@ -43,34 +44,45 @@ module.exports = {
           'css-loader',
           'sass-loader'
         ]
+      },
+      {
+        test: /\.(woff(2)?|ttf|eot|svg)(\?v=\d+\.\d+\.\d+)?$/,
+        use: [
+            {
+                loader: 'file-loader',
+                options: {
+                    name: '[name].[ext]?[hash]'
+                }
+            }
+        ]
       }
     ]
   },
   plugins: [
     new MiniCssExtractPlugin({
-      path: path.resolve(__dirname, './src/assets'),
+      path: path.resolve(__dirname, 'src/assets'),
       filename: "theme.scss.liquid"
     }),
     new UglifyJSPlugin(),
     new FileManagerPlugin({
       onStart: {
-        delete: ['./dist/**/*'], // clean dist
+        delete: ['dist/**/*'], // clean dist
       },
       onEnd: {
-        mkdir: ['./dist'],
+        mkdir: ['dist'],
         copy: [
-          { source: './src/assets', destination: './dist/assets' },
-          { source: './src/config', destination: './dist/config' },
-          { source: './src/layout', destination: './dist/layout' },
-          { source: './src/locales', destination: './dist/locales' },
-          { source: './src/sections', destination: './dist/sections' },
-          { source: './src/snippets', destination: './dist/snippets' },
-          { source: './src/templates', destination: './dist/templates' }
+          { source: 'src/assets', destination: 'dist/assets' },
+          { source: 'src/config', destination: 'dist/config' },
+          { source: 'src/layout', destination: 'dist/layout' },
+          { source: 'src/locales', destination: 'dist/locales' },
+          { source: 'src/sections', destination: 'dist/sections' },
+          { source: 'src/snippets', destination: 'dist/snippets' },
+          { source: 'src/templates', destination: 'dist/templates' }
         ],
       }
     })
   ],
   resolve: {
-    extensions: ['.css', '.scss', '.sass', '.js', '.liquid']
+    extensions: ['.css', '.scss', '.js', '.liquid']
   }
 };
